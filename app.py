8a5b145cb6755ccc8ca9ac79e1af7d38bfe5ebe7
Jorge <jhr1993@gmail.com>
2023-12-16 20:15:53 -0600

    feature commit

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ class ImageToPDFConverter:
         self.image_paths = []
         self.output_pdf_name = tk.StringVar()
         self.selected_images_listbox = tk.Listbox(root, selectmode=tk.MULTIPLE)
+        #adfasdfasdf
         
         self.initialize_ui()
 
